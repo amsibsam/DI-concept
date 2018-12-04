c630313280957485fd37e9921e24112b05c28901
Rahardyan Bisma <wb-rahardyan462662@antfin.com>
2018-12-04 09:58:27 +0700

    Initial Commit

---
+++ DI Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DI Test
+//
+//  Created by Rahardyan Bisma Setya Putra on 04/12/18.
+//  Copyright © 2018 Rahardyan Bisma Setya Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DI Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DI Test
+//
+//  Created by Rahardyan Bisma Setya Putra on 04/12/18.
+//  Copyright © 2018 Rahardyan Bisma Setya Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
